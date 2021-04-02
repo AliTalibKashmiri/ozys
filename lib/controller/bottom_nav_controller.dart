5caa1d6e936804f9b0062db075dadeaf94c99ad7
sadaqat <sadaqatdatusin@gmail.com>
2021-04-02 11:40:52 +0500

    save
    sasa

--- lib/controller/bottom_nav_controller.dart
+++ lib/controller/bottom_nav_controller.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ozys/views/screens/appoiment_page.dart';
+import 'package:ozys/views/screens/book_mark_page.dart';
+import 'package:ozys/views/screens/discovery.dart';
+import 'package:ozys/views/screens/home_page.dart';
+import 'package:ozys/views/screens/profile_page.dart';
 
 class BottomNavController extends GetxController {
   Widget currentPage;
@@ -8,6 +13,30 @@ class BottomNavController extends GetxController {
   void updatePage(index) {
     switch (index) {
       case 0:
+        currentPage = HomePage();
+        currentIndex = index;
+        update();
+        break;
+      case 0:
+        currentPage = DiscoveryPage();
+        currentIndex = index;
+        update();
+        break;
+      case 0:
+        currentPage = BookMarkPage();
+        currentIndex = index;
+        update();
+        break;
+      case 0:
+        currentPage = AppoimentPage();
+        currentIndex = index;
+        update();
+        break;
+      case 0:
+        currentPage = ProfilePage();
+        currentIndex = index;
+        update();
+        break;
     }
   }
 }
